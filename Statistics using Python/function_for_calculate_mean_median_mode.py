7b365e7935c5b6add4dcee88b61d305d39a609ac
Md. Ashiqur Rahman <62054530+ashiqur0202@users.noreply.github.com>
2023-11-14 12:04:38 +0600

    Update and rename a_function_that_takes_a_list_and_calculate_mean_median_mode.py to function_for_calculate_mean_median_mode.py

--- Statistics using Python/function_for_calculate_mean_median_mode.py
+++ Statistics using Python/function_for_calculate_mean_median_mode.py
@@ -1,15 +1,12 @@
 # Write a function that takes a list of numbers and calculates the mean, median, and mode.
 
-# Importing libraries
 import pandas as pd
 import numpy as np
 
 def calculate_mean_median_mode(list_of_numbers):
     
-  # Create a Pandas Series from the list of numbers.
   series = pd.Series(list_of_numbers)
 
-  # Calculate the mean, median, and mode of the series.
   mean = series.mean()
   median = series.median()
   mode = series.mode()
